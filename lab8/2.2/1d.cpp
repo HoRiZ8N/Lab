0a86bf3a6dd4d61a02475b9aad60a1b5710f289f
HoRiZ8N <alexpozhigan01@gmail.com>
2024-10-07 21:27:39 +0300

    Added 8.2

---
+++ lab8/2.2/1d.cpp
@@ -0,0 +1,21 @@
+#include "func.h"
+
+int main() {
+    srand(time(0));
+    int* matrix = create1dMatrix();
+    fill1dMatrix(matrix);  
+    print1dMatrix(matrix);
+    int* squaredMatrix = square1dMatrix(matrix);
+    print1dMatrix(squaredMatrix);
+    delete1dMatrix(matrix);
+    int* vect = new int[SIZE];
+    fillVector(vect);
+    printVector(vect);
+    int* ans = multiply1dMatrixVector(squaredMatrix, vect);
+    delete[] squaredMatrix;
+    delete[] vect;
+    printVector(ans);
+    delete[] ans;
+
+    return 0;
+}
