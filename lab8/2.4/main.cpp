dd699c7bfd91fb5e8b48e0fe3180af0a093671e8
HoRiZ8N <alexpozhigan01@gmail.com>
2025-02-24 13:19:38 +0300

    updated.

--- lab8/2.4/main.cpp
+++ lab8/2.4/main.cpp
@@ -1,76 +1,125 @@
 #include <iostream>
+#include <ctime>
 
-struct Node {
-    char symb;
-    Node* next;
-    Node() {
-        next = nullptr;
-    }
+enum InitializationType
+{
+    KeyboardInput = 1,
+    RandomGeneration = 2
 };
 
-void freeList(Node* head) {
-    Node* current = head;
-    Node* nextNode;
+struct Node
+{
+    char symb;
+    Node *next;
+    Node(char symbol = '\0') : symb(symbol), next(nullptr) {}
+};
 
-    while (current != nullptr) {
-        nextNode = current->next;
-        delete current;
-        current = nextNode; 
+void addNode(Node *&head, char symbol)
+{
+    Node *newNode = new Node(symbol);
+    if (!head)
+    {
+        head = newNode;
+    }
+    else
+    {
+        Node *curr = head;
+        while (curr->next != nullptr)
+        {
+            curr = curr->next;
+        }
+        curr->next = newNode;
     }
 }
 
-void printNodeVal(Node* head) {
-    Node* curr = head;
-    while (curr->next != nullptr) {
-        curr = curr->next;
+void deleteList(Node *head)
+{
+    Node *current = head;
+    Node *nextNode;
+
+    while (current != nullptr)
+    {
+        nextNode = current->next;
+        delete current;
+        current = nextNode;
+    }
+}
+
+void printNodeVal(Node *head)
+{
+    Node *curr = head;
+    while (curr != nullptr)
+    {
         std::cout << curr->symb << ' ';
-    }  
+        curr = curr->next;
+    }
     std::cout << '\n';
 }
 
-int main() {
+int main()
+{
     int size;
-    int numOfSymbols[256] = {};
-    Node* head = new Node;
-    Node* curr = head;
+    int numOfSymbols[256] = {0};
+    Node *head = nullptr;
 
     std::cout << "Enter array size: ";
-    std::cin >> size; 
-    std::cout << "\nEnter " << size << " symbols: ";
+    std::cin >> size;
+    std::cout << "Choose initialization method:\n"
+              << InitializationType::KeyboardInput << ": Keyboard\n"
+              << InitializationType::RandomGeneration << ": Random generation\n";
+    int choice;
+    std::cin >> choice;
+    InitializationType initType = static_cast<InitializationType>(choice);
 
-    for (int i = 0; i < size; i++) {
-        Node* newNode = new Node;
-        char s;
-        std::cin >> s;
-        curr->next = newNode;
-        newNode->symb = s;
-        numOfSymbols[s]++;
-        curr = curr->next;
+    switch (initType)
+    {
+    case InitializationType::RandomGeneration:
+        srand(static_cast<unsigned int>(time(0)));
+        for (int i = 0; i < size; i++)
+        {
+            char s = rand() % 94 + 33; // Limiting to printable characters
+            addNode(head, s);
+            numOfSymbols[static_cast<int>(s)]++;
+        }
+        break;
+
+    case InitializationType::KeyboardInput:
+        std::cout << "Enter " << size << " characters:\n";
+        for (int i = 0; i < size; i++)
+        {
+            char s;
+            std::cin >> s;
+            addNode(head, s);
+            numOfSymbols[static_cast<int>(s)]++;
+        }
+        break;
+
+    default:
+        std::cerr << "Invalid initialization method.\n";
+        return 1;
     }
 
     std::cout << "Values:\n";
     printNodeVal(head);
 
-    Node* newHead = new Node;
-    Node* newCurr = newHead;
-    curr = head;
-    
-    for (int i = 0; i < size; i++) {
-        if (numOfSymbols[int(curr->next->symb)] == 2) {
-            numOfSymbols[int(curr->next->symb)] = 0;
-            Node* newNode = new Node;
-            newCurr->next = newNode;
-            newNode->symb = curr->next->symb;
-            newCurr = newCurr->next;
+    Node *newHead = nullptr;
+    Node *curr = head;
+
+    while (curr != nullptr)
+    {
+        if (numOfSymbols[static_cast<int>(curr->symb)] == 2)
+        {
+            numOfSymbols[static_cast<int>(curr->symb)] = 0;
+            addNode(newHead, curr->symb);
         }
         curr = curr->next;
     }
-    
+
     std::cout << "\nSymbols that appear exactly twice:\n";
     printNodeVal(newHead);
 
-    freeList(head);
-    freeList(newHead);
+    deleteList(head);
+    deleteList(newHead);
 
     return 0;
 }
